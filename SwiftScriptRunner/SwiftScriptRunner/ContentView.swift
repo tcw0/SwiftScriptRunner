8cf525cf5b2d2bad9e2eda0f908a63b51c88a7a2
Tan-Colin Wei <tan-colin.wei@tum.de>
2024-11-02 13:14:21 +0100

    Initial commit

---
+++ SwiftScriptRunner/SwiftScriptRunner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftScriptRunner
+//
+//  Created by Tan-Colin Wei on 02.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftScriptRunner/SwiftScriptRunner/SwiftScriptRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftScriptRunnerApp.swift
+//  SwiftScriptRunner
+//
+//  Created by Tan-Colin Wei on 02.11.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftScriptRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
