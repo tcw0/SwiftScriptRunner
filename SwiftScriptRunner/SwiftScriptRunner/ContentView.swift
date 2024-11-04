f7754ff82547f705283d82524cb9411345316991
Tan-Colin Wei <tan-colin.wei@tum.de>
2024-11-04 20:09:02 +0100

    Add bracket matching

--- SwiftScriptRunner/SwiftScriptRunner/ContentView.swift
+++ SwiftScriptRunner/SwiftScriptRunner/ContentView.swift
@@ -8,6 +8,38 @@
 import SwiftUI
 import AppKit
 
+
+class CustomTextView: NSTextView {
+    let matchingPairs: [Character: Character] = [
+        "(": ")",
+        "[": "]",
+        "{": "}",
+        "\"": "\"",
+        "'": "'"
+    ]
+    
+    override func keyDown(with event: NSEvent) {
+        guard let characters = event.characters else {
+            super.keyDown(with: event)
+            return
+        }
+        
+        if let firstChar = characters.first, matchingPairs.keys.contains(firstChar) {
+            // Handle opening brackets and quotes
+            if let matchingChar = matchingPairs[firstChar] {
+                let newString = "\(firstChar)\(matchingChar)"
+                let selectedRange = self.selectedRange()
+                self.insertText(newString, replacementRange: selectedRange)
+                self.setSelectedRange(NSRange(location: selectedRange.location + 1, length: 0))
+                return
+            }
+        }
+        
+        super.keyDown(with: event)
+    }
+}
+
+
 // Custom text view for syntax highlighting using NSViewRepresentable
 struct SyntaxHighlightingTextView: NSViewRepresentable {
     @Binding var text: String
@@ -35,7 +67,6 @@ struct SyntaxHighlightingTextView: NSViewRepresentable {
         textView.isAutomaticQuoteSubstitutionEnabled = false        // Disable automatic smart quotes
         textView.isAutomaticDashSubstitutionEnabled = false         // Disable smart dashes
         textView.isAutomaticTextReplacementEnabled = false          // Disable automatic text replacements
-        textView.isAutomaticDashSubstitutionEnabled = false         // Disable dash replacement (e.g., "..." to "…")
         textView.allowsUndo = true                                  // Enable Undo Support
         
         // Wrap textView in NSScrollView to allow scrolling
